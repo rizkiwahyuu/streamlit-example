af266bb41a42753e2f49cb919b905445088f4efb
rizkiwahyuu <140294269+rizkiwahyuu@users.noreply.github.com>
2023-11-17 06:12:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,88 +3,67 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
+# Load data
 hour_data = pd.read_csv('hour.csv')
 day_data = pd.read_csv('day.csv')
 
-print("Data Per Jam:")
-print(hour_data.head())
-print("Data Per Hari:")
-print(day_data.head())
+# Drop duplicates and NaN values
 day_data.drop_duplicates(subset=['dteday'], keep='first', inplace=True)
 hour_data.dropna(inplace=True)
 day_data.dropna(inplace=True)
 
+# Sidebar
+st.sidebar.title('Bike Sharing Data Analysis')
 
-print("Informasi Umum Dataset (Harian):")
-print(day_data.info())
+# Data Overview
+st.header('Data Overview')
+st.subheader('Hourly Data')
+st.write(hour_data.head())
+st.subheader('Daily Data')
+st.write(day_data.head())
 
-print("\nStatistik Deskriptif:")
-print(day_data[['temp', 'hum', 'windspeed', 'cnt']].describe())
+# Descriptive Statistics
+st.header('Descriptive Statistics')
+st.subheader('Daily Data')
+st.write(day_data[['temp', 'hum', 'windspeed', 'cnt']].describe())
+st.subheader('Hourly Data')
+st.write(hour_data[['temp', 'hum', 'windspeed', 'cnt']].describe())
 
-plt.figure(figsize=(12, 6))
+# Distribution Plots
+st.header('Distribution Plots')
+st.subheader('Daily Data')
+fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+sns.histplot(day_data['temp'], bins=20, kde=True, ax=axes[0, 0])
+axes[0, 0].set_title('Temperature Distribution')
 
-plt.subplot(2, 2, 1)
-sns.histplot(day_data['temp'], bins=20, kde=True)
-plt.title('Distribusi Suhu (Temperature)')
+sns.histplot(day_data['hum'], bins=20, kde=True, ax=axes[0, 1])
+axes[0, 1].set_title('Humidity Distribution')
 
-plt.subplot(2, 2, 2)
-sns.histplot(day_data['hum'], bins=20, kde=True)
-plt.title('Distribusi Kelembaban (Humidity)')
+sns.histplot(day_data['windspeed'], bins=20, kde=True, ax=axes[1, 0])
+axes[1, 0].set_title('Windspeed Distribution')
 
-plt.subplot(2, 2, 3)
-sns.histplot(day_data['windspeed'], bins=20, kde=True)
-plt.title('Distribusi Kecepatan Angin (Windspeed)')
+sns.boxplot(x='cnt', data=day_data, ax=axes[1, 1])
+axes[1, 1].set_title('Bike Rental Count Boxplot')
 
-plt.subplot(2, 2, 4)
-sns.boxplot(x='cnt', data=day_data)
-plt.title('Boxplot Jumlah Penyewaan (Count)')
+st.pyplot(fig)
 
-plt.tight_layout()
-plt.show()
-
-print("Informasi Umum Dataset:")
-print(hour_data.info())
-
-print("\nStatistik Deskriptif:")
-print(hour_data[['temp', 'hum', 'windspeed', 'cnt']].describe())
-
-plt.figure(figsize=(12, 6))
-
-plt.subplot(2, 2, 1)
-sns.histplot(hour_data['temp'], bins=20, kde=True)
-plt.title('Distribusi Suhu (Temperature)')
-
-plt.subplot(2, 2, 2)
-sns.histplot(hour_data['hum'], bins=20, kde=True)
-plt.title('Distribusi Kelembaban (Humidity)')
-
-plt.subplot(2, 2, 3)
-sns.histplot(hour_data['windspeed'], bins=20, kde=True)
-plt.title('Distribusi Kecepatan Angin (Windspeed)')
-
-plt.subplot(2, 2, 4)
-sns.boxplot(x='cnt', data=hour_data)
-plt.title('Boxplot Jumlah Penyewaan (Count)')
-
-plt.tight_layout()
-plt.show()
-
+# Line Plot
+st.header('Hourly Bike Rental Patterns')
 hour_data['day_type'] = 'Working Day'
 hour_data.loc[hour_data['holiday'] == 1, 'day_type'] = 'Holiday'
 hourly_average = hour_data.groupby(['day_type', 'hr'])['cnt'].mean().unstack()
 
-plt.figure(figsize=(12, 6))
-sns.lineplot(data=hourly_average.T)
-plt.title('Perbandingan Pola Penyewaan Sepeda antara Hari Kerja dan Hari Libur')
-plt.xlabel('Jam (Hour)')
-plt.ylabel('Rata-rata Jumlah Penyewaan (Count)')
-plt.legend(title='Hari')
-plt.show()
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.lineplot(data=hourly_average.T, ax=ax)
+ax.set_title('Comparison of Bike Rental Patterns between Working Days and Holidays')
+ax.set_xlabel('Hour')
+ax.set_ylabel('Average Bike Rental Count')
+ax.legend(title='Day')
 
-import pandas as pd
-import matplotlib.pyplot as plt
+st.pyplot(fig)
 
-data = pd.read_csv("hour.csv")
+# Weather Impact
+st.header('Weather Impact on Bike Rentals')
 weather_conditions = data.groupby('weathersit').agg({'cnt': 'mean'}).reset_index()
 weather_conditions['weathersit'] = weather_conditions['weathersit'].map({
     1: 'Clear, Few clouds',
@@ -94,11 +73,11 @@ weather_conditions['weathersit'] = weather_conditions['weathersit'].map({
 })
 weather_conditions = weather_conditions.sort_values(by='cnt', ascending=False)
 
-plt.figure(figsize=(10, 6))
-plt.bar(weather_conditions['weathersit'], weather_conditions['cnt'], color='skyblue')
-plt.xlabel('Kondisi Cuaca')
-plt.ylabel('Rata-rata Jumlah Peminjaman Sepeda')
-plt.title('Pengaruh Kondisi Cuaca terhadap Jumlah Peminjaman Sepeda')
-plt.xticks(rotation=45, ha='right')  # Rotasi label agar lebih mudah dibaca
-plt.tight_layout()
-plt.show()
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.barplot(x='weathersit', y='cnt', data=weather_conditions, color='skyblue', ax=ax)
+ax.set_xlabel('Weather Conditions')
+ax.set_ylabel('Average Bike Rental Count')
+ax.set_title('Impact of Weather Conditions on Bike Rentals')
+ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+
+st.pyplot(fig)
